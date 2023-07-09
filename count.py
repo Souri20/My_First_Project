82146ef27996087ac5121cd3863cd0a79c5cc52f
Your Name <you@example.com>
2023-07-09 23:40:46 +0530

    first commit

---
+++ count.py
@@ -0,0 +1,17 @@
+my_list = [15,6,7,10,12,20,10,28,10]
+
+# num = 10
+# count=0
+#
+# for i in my_list:
+# 	if i == num:
+# 		count = count +1
+#
+# print('count of num', count )
+
+
+'method 2'
+
+a = my_list.count(10)
+print(a)
+
